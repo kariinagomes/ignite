9eb05d785855b12dcb294b2cf38104237af78467
Karina Gomes <kariinagomes_@hotmail.com>
2022-01-04 21:21:03 -0300

    feat: add Comments component

---
+++ reactjs/challenges/06-blog-com-prismic-new-feature/src/components/Comments/index.tsx
@@ -0,0 +1,20 @@
+export default function Comments(): JSX.Element {
+  return (
+    <section
+      ref={elem => {
+        if (!elem || elem.childNodes.length) {
+          return;
+        }
+        const scriptElem = document.createElement('script');
+        scriptElem.src = 'https://utteranc.es/client.js';
+        scriptElem.async = true;
+        scriptElem.crossOrigin = 'anonymous';
+        scriptElem.setAttribute('repo', 'kariinagomes/ignite');
+        scriptElem.setAttribute('issue-term', 'pathname');
+        scriptElem.setAttribute('label', 'blog-comment');
+        scriptElem.setAttribute('theme', 'github-light');
+        elem.appendChild(scriptElem);
+      }}
+    />
+  );
+}
