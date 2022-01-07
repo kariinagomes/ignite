16a4f2f3572f72b630b15d5337a5b4c735ffe7a0
Karina Gomes <kariinagomes_@hotmail.com>
2022-01-06 22:35:33 -0300

    fix: global.scss and format date

--- reactjs/challenges/06-blog-com-prismic-new-feature/src/utils/formatDate.ts
+++ reactjs/challenges/06-blog-com-prismic-new-feature/src/utils/formatDate.ts
@@ -1,7 +1,9 @@
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
-export function formatDate(date: string): string {
+export function formatDate(date?: string | null): string {
+  if (!date) return '';
+
   return format(new Date(date), 'dd MMM yyyy', {
     locale: ptBR,
   });
