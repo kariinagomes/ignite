312938a536f215efe6121bbf64654cca8d6db376
Karina Gomes <kariinagomes_@hotmail.com>
2022-01-26 21:20:20 -0300

    feat: mobile version (world trip)

--- reactjs/challenges/07-world-trip-chakra/src/components/Banner/index.tsx
+++ reactjs/challenges/07-world-trip-chakra/src/components/Banner/index.tsx
@@ -1,18 +1,23 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, Image, Text, useBreakpointValue } from '@chakra-ui/react';
 
 export function Banner() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       w='100%'
       maxWidth={1480}
-      h='80'
+      h={['60', '80']}
       mx='auto'
       bgImage='url(/images/banner-background.svg)'
       bgRepeat='no-repeat'
-      px='24'
-      py='16'
+      px={[ '4', '24']}
+      py={['10', '16']}
     >
-      <Box w='50%'>
+      <Box w={isWideVersion ? '50%' : '100%'}>
         <Text fontSize={['2xl', '4xl']} color='gray.100'>
           5 Continentes, infinitas possibilidades
         </Text>
@@ -20,14 +25,16 @@ export function Banner() {
           Chegou a hora de tirar do papel a viagem que você sempre sonhou.
         </Text>
       </Box>
-      <Box
-        w='50%'
-        display='flex'
-        justifyContent='flex-end'
-        alignItems='flex-start'
-      >
-        <Image src='/images/airplane.svg' alt='logo' />
-      </Box>
+      {isWideVersion && (
+        <Box
+          w='50%'
+          display='flex'
+          justifyContent='flex-end'
+          alignItems='flex-start'
+        >
+          <Image src='/images/airplane.svg' alt='logo' />
+        </Box>
+      )}
     </Flex>
   );
 }
--- reactjs/challenges/07-world-trip-chakra/src/components/TravelTypes/index.tsx
+++ reactjs/challenges/07-world-trip-chakra/src/components/TravelTypes/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Text } from '@chakra-ui/react';
+import { Flex, Image, Text, useBreakpointValue } from '@chakra-ui/react';
 
 const types = [
   {
@@ -29,29 +29,54 @@ const types = [
 ];
 
 export function TravelTypes() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       w='100%'
       maxWidth={1480}
-      h='40'
+      h='60'
+      mx='auto'
       my='12'
-      px='24'
+      px={['4', '24']}
       align='center'
-      justify='space-between'
+      justify={['center', 'space-between']}
+      flexWrap='wrap'
     >
-      {types.map(({ label, alt, src }) => (
-        <Flex direction='column' align='center' w="36">
-          <Image src={src} alt={alt} />
+      {types.map(({ label, alt, src }) => {
+        return isWideVersion ? (
+          <Flex direction='column' align='center' w='36'>
+            <Image src={src} alt={alt} />
+            <Text
+              fontSize={['lg', 'xl']}
+              fontWeight='600'
+              color='gray.700'
+              mt='6'
+            >
+              {label}
+            </Text>
+          </Flex>
+        ) : (
           <Text
-            fontSize={['lg', 'xl']}
+            fontSize='lg'
             fontWeight='600'
             color='gray.700'
-            mt='6'
+            minW='36'
+            align='center'
           >
+            <Image
+              src='/images/ellipse.svg'
+              alt='ellipse'
+              display='inline'
+              mr='3'
+            />
             {label}
           </Text>
-        </Flex>
-      ))}
+        );
+      })}
     </Flex>
   );
 }
