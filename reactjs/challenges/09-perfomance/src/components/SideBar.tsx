692c90aa9b22c52631290756c137e21ad9bcb215
Karina Gomes <kariinagomes_@hotmail.com>
2022-02-22 23:14:02 -0300

    feat: add challenge - perfomance

---
+++ reactjs/challenges/09-perfomance/src/components/SideBar.tsx
@@ -0,0 +1,51 @@
+import { List, ListRowRenderer } from 'react-virtualized';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  selectedGenreId: number;
+  buttonClickCallback: (args: any) => void;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  buttonClickCallback,
+}: SideBarProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <Button
+          key={String(genres[index].id)}
+          title={genres[index].title}
+          iconName={genres[index].name}
+          onClick={() => buttonClickCallback(genres[index].id)}
+          selected={selectedGenreId === genres[index].id}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        <List
+          height={500}
+          rowHeight={80}
+          width={320}
+          overscanRowCount={5}
+          rowCount={genres.length}
+          rowRenderer={rowRenderer}
+        />
+      </div>
+    </nav>
+  );
+}
