f94e0991af23a4931dfd5b25472900ac4cdbb7a2
Karina Gomes <kariinagomes_@hotmail.com>
2022-02-23 23:05:06 -0300

    feat: install jest and add test

---
+++ reactjs/chapters/03-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+import { ActiveLink } from '.';
+
+jest.mock('next/dist/client/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/',
+      };
+    },
+  };
+});
+
+describe('ActiveLink component', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <ActiveLink href='/' activeClassName='active'>
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText('Home')).toBeInTheDocument();
+  });
+
+  it('adds active link if the link as currently', () => {
+    const { getByText } = render(
+      <ActiveLink href='/' activeClassName='active'>
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText('Home')).toHaveClass('active');
+  });
+});
